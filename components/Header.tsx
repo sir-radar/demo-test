bb9e616c932915eca47ee1d6fbd3816027c4c30f
Samson Nwokike <nwokikesamson@gmail.com>
2023-06-04 00:33:54 +0100

    added header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,63 @@
 import Image from 'next/image';
 import Logo from '../assets/images/logo.svg';
+import Unboxing from '../assets/images/unboxing.svg';
+import Battles from '../assets/images/battles.svg';
+import Deals from '../assets/images/deals.svg';
+import Affiliates from '../assets/images/affiliates.svg';
+import FreeDrop from '../assets/images/freeDrop.svg';
+import Link from 'next/link';
+
+const navItems = [
+  {
+    title: 'Unboxing',
+    image: Unboxing,
+    active: false,
+  },
+  {
+    title: 'Battles',
+    image: Battles,
+    active: false,
+  },
+  {
+    title: 'Deals',
+    image: Deals,
+    active: true,
+  },
+  {
+    title: 'Affiliates',
+    image: Affiliates,
+    active: false,
+  },
+  {
+    title: 'Free Drop',
+    image: FreeDrop,
+    active: false,
+  },
+];
 
 export default function Header() {
   return (
-    <nav className="flex bg-red-500">
-      <Image src={Logo} alt="Logo" />
+    <nav className="flex py-2 px-3.5 bg-white">
+      <Link href="/" className="flex items-center justify-center">
+        <svg className="w-[131px] mx-4 my-0" viewBox="0 -2 50 9">
+          <Logo />
+        </svg>
+      </Link>
 
-      <ul className="flex">
-        <li>Unboxing</li>
-        <li>Battles</li>
-        <li>Deals</li>
-        <li>Affiliates</li>
-        <li>Free Drop</li>
+      <ul className="flex font-bold uppercase">
+        {navItems.map((item) => (
+          <li
+            key={item.title}
+            className={
+              item.active
+                ? 'flex items-center px-4 py-5 text-[#3D789D]'
+                : 'flex items-center px-4 py-5 text-[#8E8E99]'
+            }
+          >
+            <item.image className="mr-[0.65rem]" />
+            <p>{item.title}</p>
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Header from '@/components/Header';
-import { Inter } from 'next/font/google';
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="bg-[#F0F0F2]">
       {/* <div className="wheel">
         <div className="spinner"></div>
       </div> */}
       <Header />
+
+      <div className="flex min-h-screen px-6 py-[20px]">
+        <section>
+          <h2>Available Items</h2>
+        </section>
+        <section></section>
+      </div>
     </main>
   );
 }
