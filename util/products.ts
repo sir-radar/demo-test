0e6ce511120458d4020684370f7a43cb60509510
Samson Nwokike <nwokikesamson@gmail.com>
2023-06-07 04:21:04 +0100

    product update

--- util/products.ts
+++ util/products.ts
@@ -1,27 +1,13 @@
 export const products = [
   {
     id: 1,
-    name: 'Royal Oak Frosted Gold Double Balance...',
-    price: 890520,
-    brand: 'Audemars Piguet',
-    imageURL: '/images/products/openworked.png',
-  },
-  {
-    id: 2,
-    name: 'Astronomia Clarity',
-    price: 600000,
-    brand: 'Jacob & CO ',
-    imageURL: '/images/products/astronomia.png',
-  },
-  {
-    id: 3,
     name: 'Daytona Rainbow',
     price: 637329,
     brand: ' Rolex  ',
     imageURL: '/images/products/rolex.png',
   },
   {
-    id: 4,
+    id: 2,
     name: '765LT Spider',
     price: 611843,
     brand: 'McLaren',
